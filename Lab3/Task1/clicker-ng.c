cd6e47478fa90e71a4d899a93dbf0023d1a2bb6f
Hani-design <79083778+Hani-design@users.noreply.github.com>
2024-02-22 20:09:25 +0100

    order

--- Lab3/Task1/clicker-ng.c
+++ Lab3/Task1/clicker-ng.c
@@ -28,12 +28,6 @@ static uint8_t unique_node_count = 0;
 // Function to handle event and check for alarm triggering
 void handle_event(const linkaddr_t *src) {
 
-    //Clear event history between alarms, constraint for alarm delays is 30 seconds
-    clock_time_t alarm_interval = (clock_time_t)(event_history[MAX_NUMBER_OF_EVENTS - 1].time - clock_time());
-    if (alarm_interval > (clock_time_t)30 * CLOCK_SECOND) {
-        unique_node_count = 0;
-    }
-
     static int event_count = 0;
     if (event_count < MAX_NUMBER_OF_EVENTS) {
         // Check if the source node is already recorded in event history
@@ -71,6 +65,12 @@ void handle_event(const linkaddr_t *src) {
       } 
     }
 
+    //Clear event history between alarms, constraint for alarm delays is 30 seconds
+    clock_time_t alarm_interval = (clock_time_t)(event_history[MAX_NUMBER_OF_EVENTS - 1].time - clock_time());
+      if (alarm_interval > (clock_time_t)30 * CLOCK_SECOND) {
+        unique_node_count = 0;
+        event_count = 0;
+    }
     
     // // Clear event history if maximum number of events is reached
     // if (event_count >= MAX_NUMBER_OF_EVENTS) {
