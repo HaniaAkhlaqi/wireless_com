aadd2f7cca03179ca4b8aee5d84a769191ea7dc0
Hani-design <79083778+Hani-design@users.noreply.github.com>
2024-02-23 13:08:04 +0100

    lets test

--- Lab3/Task1/clicker-ng.c
+++ Lab3/Task1/clicker-ng.c
@@ -25,8 +25,8 @@ struct event event_history[MAX_NUMBER_OF_EVENTS];
 // Variable to track the number of unique nodes that have triggered events
 static uint8_t unique_node_count = 0;
 
-// Timer for resetting the alarm state
-static struct etimer alarm_reset_timer;
+//variable to track if alarm is triggered
+static uint8_t alarm_triggered = 0;
 
 // Function to handle event and check for alarm triggering
 void handle_event(const linkaddr_t *src) {
@@ -39,6 +39,7 @@ void handle_event(const linkaddr_t *src) {
         for (i = 0; i < event_count; i++) {
             if (linkaddr_cmp(src, &event_history[i].addr)) {
                 exists = 1;
+                event_history[i].time = clock_time();
                 break;
             }
         }
@@ -52,23 +53,25 @@ void handle_event(const linkaddr_t *src) {
             printf("New event registered from Node %d\n", src->u8[0]);
         }
     }
-        // Check if alarm should be triggered
+    // Check if alarm should be triggered when 3 unique nodes have triggered events
     if (unique_node_count >= MAX_NUMBER_OF_EVENTS) {
       clock_time_t event_duration = (clock_time_t)(event_history[MAX_NUMBER_OF_EVENTS - 1].time - event_history[0].time);
       printf("Event duration: %lu\n", event_duration);
       if (event_duration < (clock_time_t)30 * CLOCK_SECOND) {
+        alarm_triggered = 1;
         leds_toggle(LEDS_YELLOW);
         leds_toggle(LEDS_BLUE);
         printf("ALARM triggered!\n");
-        etimer_set(&alarm_reset_timer, CLOCK_SECOND * 30);
-      }
-      static int i = 0;
-      for(i = 0; i < MAX_NUMBER_OF_EVENTS-1; i++) {
+      } else {
+        alarm_triggered = 0;
+        static int i = 0;
+        for(i = 0; i < MAX_NUMBER_OF_EVENTS-1; i++) {
         event_history[i] = event_history[i+1];
+        event_count--;
         printf("removed old event\n");
-      } 
-
-
+        } 
+      }
+     
     }
 
     // // Clear event history if maximum number of events is reached & time between alarms is too long
@@ -111,16 +114,6 @@ PROCESS_THREAD(clicker_ng_process, ev, data) {
 
     while (1) {
         PROCESS_WAIT_EVENT_UNTIL(ev == sensors_event && data == &button_sensor);
-
-        
-        // Check if the alarm reset timer has expired
-        if (etimer_expired(&alarm_reset_timer)) {
-            unique_node_count = 0;
-            printf("Alarm reset\n");
-            leds_off(LEDS_GREEN);
-            leds_off(LEDS_BLUE);
-            leds_off(LEDS_YELLOW);
-        }
 
         memcpy(nullnet_buf, &payload, sizeof(payload));
         nullnet_len = sizeof(payload);
